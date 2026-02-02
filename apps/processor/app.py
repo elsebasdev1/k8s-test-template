ea601e706727daf92d015134219909e07438b178
rafael2000-prog <146790773+rafael2000-prog@users.noreply.github.com>
2026-02-02 16:25:38 -0500

    Integrate Redis for value persistence and update response
    Added Redis caching for processed values and improved response formatting.

--- apps/processor/app.py
+++ apps/processor/app.py
@@ -1,15 +1,20 @@
-import os, requests
-from flask import Flask, request
+import os, requests, redis
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 NEXT_HOP = os.getenv("NEXT_SERVICE_URL", "http://auditor-svc:5000")
+# Conexión a la BD Redis (nombre del servicio en K8s)
+DB_HOST = os.getenv("REDIS_HOST", "redis-svc")
+
+# Conectamos a Redis
+cache = redis.Redis(host=DB_HOST, port=6379, decode_responses=True)
 
 @app.route('/process', methods=['POST'])
 def process():
     data = request.json
     val = data.get('value', 0)
     
-    # Lógica de Negocio (Fácil de cambiar en el examen)
+    # Lógica de Negocio
     if val % 2 == 0:
         data['value'] = val * 2
         data['audit'].append("Multiplied by 2 (Even)")
@@ -17,9 +22,17 @@ def process():
         data['value'] = val + 1
         data['audit'].append("Added 1 (Odd)")
 
+    # --- PERSISTENCIA: Guardar en Redis ---
+    try:
+        cache.set("ultimo_valor", data['value'])
+        print(f"Guardado en Redis: {data['value']}", flush=True)
+    except Exception as e:
+        print(f"Error Redis: {e}", flush=True)
+    # --------------------------------------
+
     # Pasar al siguiente
     response = requests.post(f"{NEXT_HOP}/audit", json=data)
-    return response.json()
+    return jsonify(response.json())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
